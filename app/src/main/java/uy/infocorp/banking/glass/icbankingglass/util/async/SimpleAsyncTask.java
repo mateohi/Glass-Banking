5d3e6582e44dbc2b02d8db6e22de5aeae4aee120
Mateo Hermida <mateohermida@outlook.com>
2014-08-20 23:01:58 -0300

    Agrego clases utilitarias para manejo de tareas asyncrónicas, bitmaps y Toasts

---
+++ app/src/main/java/uy/infocorp/banking/glass/icbankingglass/util/async/SimpleAsyncTask.java
@@ -0,0 +1,17 @@
+package uy.infocorp.banking.glass.icbankingglass.util.async;
+
+import android.os.AsyncTask;
+
+public abstract class SimpleAsyncTask<T> extends AsyncTask<Object, Void, T>{
+
+    private FinishedTaskListener listener;
+
+    public SimpleAsyncTask(FinishedTaskListener<T> listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected void onPostExecute(T result) {
+        listener.onResult(result);
+    }
+}
