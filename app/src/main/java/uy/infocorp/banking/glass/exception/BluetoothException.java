46a97e15e066c55e447ab95bea2e34a32461a41b
Mateo Hermida <mateohermida@outlook.com>
2014-10-11 18:13:22 -0200

    agrego excepcion para manejo de bluetooth

---
+++ app/src/main/java/uy/infocorp/banking/glass/exception/BluetoothException.java
@@ -0,0 +1,8 @@
+package uy.infocorp.banking.glass.exception;
+
+public class BluetoothException extends RuntimeException {
+
+    public BluetoothException(String message) {
+        super(message);
+    }
+}
