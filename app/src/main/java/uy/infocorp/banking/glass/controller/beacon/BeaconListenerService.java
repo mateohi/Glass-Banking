794cb849591de68fbe99331e14e56eb590e90a25
Mateo Hermida <mateohermida@outlook.com>
2014-10-12 21:36:04 -0200

    agrego flag de new activity

--- app/src/main/java/uy/infocorp/banking/glass/controller/beacon/BeaconListenerService.java
+++ app/src/main/java/uy/infocorp/banking/glass/controller/beacon/BeaconListenerService.java
@@ -3,6 +3,7 @@ package uy.infocorp.banking.glass.controller.beacon;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import uy.infocorp.banking.glass.controller.beacon.rate.BranchRatingActivity;
 import uy.infocorp.banking.glass.domain.beacon.BeaconHandler;
@@ -11,6 +12,7 @@ import uy.infocorp.banking.glass.domain.beacon.estimote.EstimoteBeaconHandler;
 
 public class BeaconListenerService extends Service {
 
+    private static final String TAG = BeaconListenerService.class.getSimpleName();
     private BeaconHandler beaconHandler;
 
     @Override
@@ -18,6 +20,7 @@ public class BeaconListenerService extends Service {
         this.beaconHandler = new EstimoteBeaconHandler(this.getApplicationContext(), new PlaceListener() {
             @Override
             public void onEntered(String placeId) {
+                Log.i(TAG, "Entered " + placeId);
                 // show welcome message, inform bank branch, etc..
             }
 
@@ -43,6 +46,7 @@ public class BeaconListenerService extends Service {
 
     private void startRatingActivity(String branchId) {
         Intent ratingIntent = new Intent(this, BranchRatingActivity.class);
+        ratingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         ratingIntent.putExtra(BranchRatingActivity.BRANCH_ID, branchId);
 
         startActivity(ratingIntent);
