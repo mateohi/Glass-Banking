8e6bf24eb2700bd35face30e2f28ca00821e719a
Mateo Hermida <mateohermigle@gmail.com>
2014-10-30 00:41:53 -0200

    agrego dateUtils para manejo de fechas iso

---
+++ app/src/main/java/uy/infocorp/banking/glass/util/date/DateUtils.java
@@ -0,0 +1,16 @@
+package uy.infocorp.banking.glass.util.date;
+
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+
+import java.util.Date;
+
+public class DateUtils {
+
+    private static final DateTimeFormatter ISO_FORMAT = ISODateTimeFormat.dateTime();
+
+    public static String dateToIsoString(Date date) {
+        long dateInMillis = date.getTime();
+        return ISO_FORMAT.print(dateInMillis);
+    }
+}
