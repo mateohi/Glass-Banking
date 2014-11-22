d93c755838e27f99b69ca08f02b9e01658ecb9d3
Mateo Hermida <mateohermigle@gmail.com>
2014-11-22 15:53:23 -0200

    saco comentario vergonzoso

--- app/src/main/java/uy/infocorp/banking/glass/integration/Constants.java
+++ app/src/main/java/uy/infocorp/banking/glass/integration/Constants.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 public class Constants {
 
     public static final Date DATE_MIN_VALUE = new Date(Long.MIN_VALUE);
-    public static final boolean OFFLINE_MODE = true;// {true, false}
+    public static final boolean OFFLINE_MODE = true;
     public static final String X_AUTH_TOKEN_HEADER_NAME = "X-Auth-Token";
 
     //Transfer History
